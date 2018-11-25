60c6bd02a3e12e4e8a970abfb4893e164fe1f041
LiTtleBayReal <zhouxiaobei1993@163.com>
2018-11-26 01:37:11 +0800

    完成个人中心 客服 我的订单部分页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,7 @@ var qqmapsdk;
 var that;
 Page({
   data: {
-    titleData: ['出租车', '网约车', '电话招车', '出租车', '网约车', '电话招车'],
+    titleData: ['出租车', '网约车', '电话招车'],
     navScrollLeft: 0,
     //记录正在显示的页面标签
     currentTab: 1,
@@ -29,21 +29,42 @@ Page({
     this.mapCtx = wx.createMapContext("99CallCarMap"); // 地图组件的id
     // this.movetoPosition()
   },
+  switchNav:function(e){
+    console.log(e.currentTarget.dataset.id);
+    var ct = e.currentTarget.dataset.id;
+    that.setData({
+      currentTab:ct
+    })
+  },
   toggleListener: function(e) {
-    switch (e._relatedInfo.anchorTargetText) {
-      case "现在":
+    switch (e.target.id) {
+      case "nowBtn":
         console.log("现在");
         that.setData({
           isNow: 0
         })
         break;
-      case "预约":
+      case "orderBtn":
         console.log("预约");
         that.setData({
           isNow: 1
         })
         break
     }
+  },
+  //点击选择框确定后返回的参数
+  bindStartMultiPickerChange:function(e){
+     var v = [];
+     v = e.detail.value;
+     var day = that.data.multiArray[0][v[0]];
+     var hour = that.data.multiArray[1][v[1]];
+     var minute = that.data.multiArray[2][v[2]];
+     that.setData({
+       order_times:day+"-"+hour+":"+minute
+     })
+  },
+  bindMultiPickerColumnChange:function(e){
+    
   },
   orderTimeListener: function(e) {
     var date = new Date();
---
+++ pages/myschedule/myschedule.js
@@ -0,0 +1,118 @@
+// pages/myschedule/myschedule.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isNoData:true,
+    radioValues: [{
+        'value': '网约车行程',
+        'selected': true
+      },
+      {
+        'value': '出租车行程',
+        'selected': false
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.clazzStatus();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+  indexChanged: function(e) {
+    // 点中的是组中第个元素 
+    var index = e.target.dataset.index;
+    // 读取原始的数组 
+    var radioValues = this.data.radioValues;
+    for (var i = 0; i < radioValues.length; i++) {
+      // 全部改为非选中 
+      radioValues[i].selected = false;
+      // 当前那个改为选中 
+      radioValues[index].selected = true;
+    }
+    // 写回数据 
+    this.setData({
+      radioValues: radioValues
+    });
+    // clazz状态 
+    this.clazzStatus();
+  },
+  clazzStatus: function() {
+    /* 此方法分别被加载时调用，点击某段时调用 */ // class样式表如"selected last","selected" 
+    var clazz = [];
+    // 参照数据源
+    var radioValues = this.data.radioValues;
+    for (var i = 0; i < radioValues.length; i++) {
+      // 默认为空串，即普通按钮 
+      var cls = '';
+      // 高亮，追回selected 
+      if (radioValues[i].selected) {
+        cls += 'selected ';
+      }
+      // 最后个元素, 追加last 去掉右侧的竖线
+      if (i == radioValues.length - 1) {
+        cls += 'last ';
+      }
+      //去掉尾部空格 
+      cls = cls.replace(/(\s*$)/g, '');
+      clazz[i] = cls;
+    }
+    // 写回数据 
+    this.setData({
+      clazz: clazz
+    });
+  }
+})
