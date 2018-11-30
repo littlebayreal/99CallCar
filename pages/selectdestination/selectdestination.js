ee3013c366c869ec0beaf2f182a5fb2dcfd97c13
littlebayreal <zhouxiaobei1993@163.com>
2018-11-30 17:18:10 +0800

    首页的车辆选择

--- pages/selectdestination/selectdestination.js
+++ pages/selectdestination/selectdestination.js
@@ -7,64 +7,67 @@ Page({
    * 页面的初始数据
    */
   data: {
-    addressList: [{
+    //显示队列
+    addressList: null,
+    //缓存队列
+    storageList: [{
         addressName: '三亚站',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '三亚凤凰国际机场',
-        addressInfo: ''
+        addressInfo: '',
       }, {
         addressName: '解放路步行街',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '三亚明珠广场',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '三亚农垦医院',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '三亚人民医院',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '金润阳光购物广场',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '大菠萝广场',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '大东海旅游区',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '亚龙湾站',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '第一市场',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '友谊路下岗职工海鲜加工广场',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '春园海鲜广场',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '天涯海角',
-        addressInfo: ''
+        addressInfo: '',
       },
       {
         addressName: '南山寺',
-        addressInfo: ''
+        addressInfo: '',
       }
     ]
   },
@@ -73,25 +76,36 @@ Page({
     var currentPage = pages[pages.length - 1] // 获取当前页面
     var prevPage = pages[pages.length - 2] //获取上一个页面
     　　 // 设置上一个页面的数据（可以修改，也可以新增
-    switch(that.data.type){
+    switch (that.data.type) {
       case 0:
         prevPage.setData({
           origin: e.currentTarget.dataset.item.addressName
         })
-      break;
+        break;
       case 1:
         prevPage.setData({
           destination: e.currentTarget.dataset.item.addressName
         })
-      break;
+        break;
     }
-   // 返回上一个页面（这个API不允许跟参数）
+    //更新原始数据  记录搜索记录
+    var ads = that.data.storageList;
+    var newLength = ads.unshift(e.currentTarget.dataset.item);
+    console.log(newLength);
+    for (var i = 0; i < ads.length; i++) {
+      console.log(ads[i].addressName);
+    }
+    ads = ads.slice(0, 10);
+    that.setData({
+      addressList: ads,
+      storageList: ads
+    })
+    // 返回上一个页面（这个API不允许跟参数）
     wx.navigateBack({
       delta: 1
     })
   },
   inputChangeListener: function(e) {
-    console.log(e);
     qqmapsdk.getSuggestion({
       region: "苏州",
       keyword: e.detail.value,
@@ -100,14 +114,15 @@ Page({
         var al = [];
         var datas = res.data;
         for (var i = 0; i < 10; i++) {
-          var data={
-            addressName:datas[i].title,
-            addressInfo:datas[i].address
+          var data = {
+            addressName: datas[i].title,
+            addressInfo: datas[i].address,
+            update: false
           }
           al.push(data);
         }
         that.setData({
-          addressList:al
+          addressList: al
         })
       },
 
@@ -123,10 +138,26 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    that = this;
+    //默认读取原始数据
+    that.setData({
+      addressList: that.data.storageList
+    })
+    wx.getStorage({
+      key: 'address_list',
+      success: function(res) {
+        console.log(res);
+        that.setData({
+          addressList: res.data,
+          storageList: res.data
+        })
+      },
+    })
+
     qqmapsdk = new QQMapWX({
       key: 'JVCBZ-5UK6J-TQGFH-FWJO6-WECYF-GJFIF'
     });
-    that = this;
+
     switch (options.type) {
       case '0':
         console.log("没走到吗");
@@ -169,7 +200,10 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function() {
-
+    wx.setStorage({
+      key: 'address_list',
+      data: that.data.storageList,
+    })
   },
 
   /**
